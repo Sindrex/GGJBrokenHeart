be8ab5f549c3093688d2ec1805df881be1898922
SindreP <sindrhpa@stud.ntnu.no>
2020-01-31 21:35:14 +0100

    frog anims

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -20,6 +20,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * speed, Input.GetAxis("Vertical") * Time.deltaTime * speed, 0.0f);
+        /*
         if (Input.GetKey(KeyCode.W))
         {
             this.gameObject.transform.Translate(new Vector3(0, 1) * speed * Time.deltaTime);
@@ -35,6 +37,6 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKey(KeyCode.D))
         {
             this.gameObject.transform.Translate(new Vector3(1, 0) * speed * Time.deltaTime);
-        }
+        }*/
     }
 }
