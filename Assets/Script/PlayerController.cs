0c2c325e40a46e3c58bace93d85916119ee1c1ef
SindreP <sindrhpa@stud.ntnu.no>
2020-01-31 20:35:54 +0100

    frog with walk

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.gameObject.transform.Translate(new Vector3(0, 1) * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.gameObject.transform.Translate(new Vector3(-1, 0) * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.gameObject.transform.Translate(new Vector3(0, -1) * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.gameObject.transform.Translate(new Vector3(1, 0) * speed * Time.deltaTime);
+        }
+    }
+}
