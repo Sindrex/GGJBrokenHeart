856ca9b274bc13c1a6c224e2a3b2becc460b375d
anetteOlli <anette.olli.siiri@hotmail.com>
2020-02-01 19:47:57 +0100

    wasp in progress

--- Assets/Script/WaspAI.cs
+++ Assets/Script/WaspAI.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class WaspAI : MonoBehaviour
 {
+    public PlayerController myPlayer;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +15,16 @@ public class NewBehaviourScript : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("wasp");
+
+        if (collision.gameObject.name.Equals("Frog"))
+        {
+            print("wasp sting");
+
+        }
     }
 }
