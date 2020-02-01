d21fd9acfc481944e4b742f0e233e09f60a9db89
SindreP <sindrhpa@stud.ntnu.no>
2020-02-01 19:49:44 +0100

    flyAI

--- Assets/Script/FlyAI.cs
+++ Assets/Script/FlyAI.cs
@@ -5,17 +5,40 @@ using UnityEngine;
 public class FlyAI : MonoBehaviour
 {
     public float speed;
-    private Vector3 startPos;
+    public Vector3 startPos;
+    public float radius = 5;
+    public int direction = 1;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPos = transform.position;
     }
 
     private void FixedUpdate()
     {
-        
+        if(direction > 0)
+        {
+            if (startPos.x + radius > transform.position.x)
+            {
+                transform.Translate(new Vector3(1, 0, 0) * speed * Time.deltaTime);
+            }
+            else
+            {
+                direction = -1;
+            }
+        }
+        else
+        {
+            if (startPos.x - radius < transform.position.x)
+            {
+                transform.Translate(new Vector3(-1, 0, 0) * speed * Time.deltaTime);
+            }
+            else
+            {
+                direction = 1;
+            }
+        }
     }
 
     // Update is called once per frame
