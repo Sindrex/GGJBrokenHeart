58f1e288cd9ba88294e0d73fc61575f7828899e5
SindreP <sindrhpa@stud.ntnu.no>
2020-02-01 18:06:33 +0100

    wasp and stuff

---
+++ Assets/Script/FlyAI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyAI : MonoBehaviour
+{
+    public float speed;
+    private Vector3 startPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Script/TongueController.cs
+++ Assets/Script/TongueController.cs
@@ -1,21 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TongueController : MonoBehaviour
 {
     public PlayerController myPlayer;
+    public Vector2 hookPos;
+    public bool hookLerp;
+    public float lerpFac;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        //Vector3 playerPos = myPlayer.transform.position;
+        //float x = Mathf.Lerp(playerPos.x, hookPos.y, lerpFac);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -26,5 +31,7 @@ public class TongueController : MonoBehaviour
             myPlayer.lifes++;
             Destroy(collision.gameObject);
         }
+        //hookPos = collision.transform.position;
+        //hookLerp = true;
     }
 }
