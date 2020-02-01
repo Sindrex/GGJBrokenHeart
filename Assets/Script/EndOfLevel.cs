83fb2e62c45ce22d38aac911f550e67237a8d497
sindrtho <sindrtho@hotmail.com>
2020-02-01 18:06:23 +0100

    Added buttons, doors and end of level!

---
+++ Assets/Script/EndOfLevel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndOfLevel : MonoBehaviour
+{
+    public string NEXT_LEVEL;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        print("TRIGGERED!");
+        if (other.gameObject.name.Equals("Frog"))
+        {
+            print("FINISHED!");
+            SceneManager.LoadScene(NEXT_LEVEL);
+        }
+    }
+}
